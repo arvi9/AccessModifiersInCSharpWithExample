e34507352af3f4733804bf3a7f6fe6b518e12175
adc work <adc9work@outlook.com>
2018-10-02 13:28:53 +0530

    Commit All

--- AccessModifiersInCSharpWithExample/Program.cs
+++ AccessModifiersInCSharpWithExample/Program.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibraryAssembly;
 
 namespace AccessModifiersInCSharpWithExample
 {
+     class DerivedClass : AssemblyClass
+    {
+
+       static void fun()
+        {
+            AssemblyClass.protectedClass.protectedClassFunction();
+            AssemblyClass.protectedInternalClass.protectedInternalClassFunction();
+            AssemblyClass.publicClass.publicClassFunction();
+
+        }
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            AssemblyClass ac = new AssemblyClass();
+            DerivedClass dc = new DerivedClass();
+            AssemblyClass.publicClass.publicClassFunction();
         }
     }
 }
---
+++ ClassLibrary1/AssemblyClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ClassLibraryAssembly
+{
+    public class AssemblyClass
+    {
+
+        public class publicClass
+        {
+            public static void publicClassFunction()
+            {
+                Console.WriteLine("publicClassFunction");
+            }
+
+        }
+
+        private class privateClass
+        {
+
+            public static void privateClassFunction()
+            {
+                Console.WriteLine("privateClassFunction");
+            }
+        }
+
+        internal class internalClass
+        {
+            public static void internalClassFunction()
+            {
+                Console.WriteLine("internalClassFunction");
+            }
+
+        }
+
+
+
+
+        protected class protectedClass
+        {
+            public static void protectedClassFunction()
+            {
+                Console.WriteLine("protectedClassFunction");
+            }
+
+        }
+
+        protected internal class protectedInternalClass
+        {
+            public static void protectedInternalClassFunction()
+            {
+                Console.WriteLine("protectedInternalClassFunction");
+            }
+
+        }
+
+
+    }
+}
